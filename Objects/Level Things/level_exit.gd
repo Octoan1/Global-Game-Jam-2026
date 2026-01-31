c3f86758cd03fb2991d942642239753936a0bb2f
Connradical <conneremoore@gmail.com>
2026-01-31 17:25:37 -0600

    Level Load System
    - Levels can be completed
    - Next level will be loaded after completion

--- Objects/Level Things/level_exit.gd
+++ Objects/Level Things/level_exit.gd
@@ -3,7 +3,18 @@ extends Area2D
 var player1_detected = false
 var player2_detected = false
 
-signal exit_level
+signal exit_level(next: int, current: int)
+
+@export var next_level_id: int
+#@export var current_level_id: int
+var player1_pos: Vector2
+var player2_pos: Vector2
+
+func _ready():
+	var head_node = get_parent()
+	print(head_node.name)
+	player1_pos = get_parent().get_player_1_spawn()
+	player2_pos = get_parent().get_player_2_spawn()
 
 func _on_body_entered(body):
 	print(body.name)
@@ -18,7 +29,10 @@ func _on_body_entered(body):
 	
 	if(player1_detected and player2_detected):
 		print("Exiting level.")
-		emit_signal("exit_level")
+		print(player1_pos)
+		print(player2_pos)
+		exit_level.emit(next_level_id, player1_pos, player2_pos)
+		#queue_free()
 
 
 func _on_body_exited(body):
---
+++ Scenes/Levels/level_data.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@export var player_1_spawn: Vector2
+@export var player_2_spawn: Vector2
+
+func get_player_1_spawn():
+	return player_1_spawn
+
+func get_player_2_spawn():
+	return player_2_spawn
