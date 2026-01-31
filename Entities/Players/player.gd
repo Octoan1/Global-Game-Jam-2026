560ef9366c9c744c055f4680e878c2b3ea69301d
jprei <joeprei20@gmail.com>
2026-01-31 09:44:39 -0600

    More player control

--- Entities/Players/player.gd
+++ Entities/Players/player.gd
@@ -2,16 +2,16 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
-const GSPEED = 5
+const GSPEED = 3
 const JUMP_VELOCITY = -400.0
-@onready var mask = false
+@export var mask = true
 @onready var throwing = false
 @export var pointer_scene: PackedScene
 @export var arrow_scene: PackedScene
 
 @export var thickness := 2.0
 @export var max_length := 50.0
-@export var joystick_id := 1
+@export var joystick_id := 0
 @export var deadzone := 0.2
 @export var arrow_color := Color.BLUE
 
@@ -19,18 +19,25 @@ var arrow_dir = Vector2.ZERO
 var arrow_length = 0.0
 
 func _physics_process(delta: float) -> void:
+	if mask:
+		self.set_collision_layer_value(1, true)
+		self.set_collision_layer_value(2, false)
+	else:
+		self.set_collision_layer_value(2, true)
+		self.set_collision_layer_value(1, false)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
 
 	# Handle jump.
-	if Input.is_action_pressed("p2_jump") and is_on_floor() and not throwing:
+	if Input.is_action_pressed("p1_jump" if joystick_id == 0 else "p2_jump") and is_on_floor() and not throwing:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("p2_move_left", "p2_move_right")
+	var direction := Input.get_axis("p1_move_left" if joystick_id == 0 else "p2_move_left", "p1_move_right" if joystick_id == 0 else "p2_move_left")
 	if direction and not throwing and mask:
 		velocity.x = direction * SPEED
 	else:
@@ -47,9 +54,9 @@ func _physics_process(delta: float) -> void:
 		self.global_position.y += stick.y * GSPEED
 		
 		
-	if Input.is_action_just_pressed("p2_throw") and mask:
+	if Input.is_action_just_pressed("p1_throw" if joystick_id == 0 else "p2_throw") and mask:
 		throwing = true
-	if Input.is_action_just_pressed("p2_back") and throwing:
+	if Input.is_action_just_pressed("p1_back" if joystick_id == 0 else "p2_back") and throwing:
 		throwing = false
 		
 	_update_arrow()
