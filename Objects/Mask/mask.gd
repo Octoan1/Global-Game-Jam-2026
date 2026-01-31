5ace9fca325468940e34a30f692717a5d4a30974
jprei <joeprei20@gmail.com>
2026-01-31 14:52:42 -0600

    Again

--- Objects/Mask/mask.gd
+++ Objects/Mask/mask.gd
@@ -29,7 +29,6 @@ func _physics_process(_delta: float) -> void:
 
 func _on_body_entered(body):
 	if body.name == "Player2" or body.name == "Player1":
-		print("hit")
 		emit_signal("hit_player", body)
 		queue_free()
 		returning = false
