5febb781bb0dd12c2e1ceeb7753226be9ad7e544
Connradical <conneremoore@gmail.com>
2026-01-31 23:34:49 -0600

    Level Complete changes
    - Added sfx to reaching the end goal, as both dingle and doo
    - added a delay for smoother level transition

--- Objects/Level Things/level_exit.gd
+++ Objects/Level Things/level_exit.gd
@@ -12,6 +12,12 @@ signal exit_level(next: int, current: int)
 var player1_pos: Vector2
 var player2_pos: Vector2
 
+@onready var doo_complete = $DooComplete
+@onready var dingle_complete = $DingleComplete
+
+var await_dingle_sound = false
+var await_doo_sound = false
+
 func _ready():
 	var head_node = get_parent()
 	print(head_node.name)
@@ -24,12 +30,25 @@ func _ready():
 func _on_body_entered(body):
 	if activate_exit == true:
 		if body.name == "Player1":
+			if await_dingle_sound == false:
+				dingle_complete.play()
+				await_dingle_sound = true
+				await get_tree().create_timer(1).timeout
+				await_dingle_sound = false
 			player1_detected = true
+	
+	
 		if body.name == "Player2":
+			if await_doo_sound == false:
+				doo_complete.play()
+				await_doo_sound = true
+				await get_tree().create_timer(1).timeout
+				await_doo_sound = false
 			player2_detected = true
 		
 	
 	if(player1_detected and player2_detected):
+		await get_tree().create_timer(1).timeout
 		exit_level.emit(next_level_id, player1_pos, player2_pos)
 		print("Going to level: ", next_level_id)
 		#queue_free()
