78fddfef9da78d65e2246e2dd317d0d1235b5c50
jprei <joeprei20@gmail.com>
2026-02-01 00:51:58 -0600

    Fix maybe

--- Objects/Pile/pile.gd
+++ Objects/Pile/pile.gd
@@ -1,13 +1,13 @@
 extends RigidBody2D
 
-@export var fly_speed = 125.0
+@export var fly_speed = 200.0
 var player: CharacterBody2D
 var flying = false
 var connected = false
 var red: bool
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 		
-func _process(delta):
+func _physics_process(delta: float):
 	if not connected:
 		player.gather_leaves.connect(_on_player_gather_leaves)
 		connected = true
@@ -27,7 +27,7 @@ func _process(delta):
 		player.unpoof_from_leaves()
 		return
 
-	global_position += dir.normalized() * fly_speed * delta
+	linear_velocity = dir / distance * fly_speed
 	rotation = dir.angle()
 
 func _on_player_gather_leaves():
