ed2c136a62f219edddfa60d5fe6b8f0f6ef50dc1
Connradical <conneremoore@gmail.com>
2026-01-31 23:18:58 -0600

    Level 2 Changes
    - Death sfx added
    - Death rework. Now only the player who dies resets, and there is a small cooldown.

---
+++ Objects/Level Things/death_plane.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+signal player1_reset
+signal player2_reset
+
+@onready var dingle_dies = $DingleDies
+@onready var doo_dies = $DooDies
+
+var sound_ready = true
+
+func _on_body_entered(body):
+	
+		if body.name == "Player1":
+			#await get_tree().create_timer(0.5).timeout
+			#player_reset.emit()
+			if sound_ready == true:
+				dingle_dies.play()
+				sound_ready = false
+				await get_tree().create_timer(1).timeout
+				sound_ready = true
+			player1_reset.emit()
+		
+		
+		if body.name == "Player2":
+			#await get_tree().create_timer(0.5).timeout
+			#player_reset.emit()
+			if sound_ready == true:
+				doo_dies.play()
+				sound_ready = false
+				await get_tree().create_timer(1).timeout
+				sound_ready = true
+			player2_reset.emit()
